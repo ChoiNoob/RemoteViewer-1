61eb18e8e8a7733048ea6d00743a8fbeaf916398
das <damintsew@gmail.com>
2013-07-31 23:26:23 +0400

    Тестовая рабочая таблица + попытки телнета
    Сейчас надо передеолать все под новую парадигму

---
+++ src/main/java/com/damintsev/client/dao/Device.java
@@ -0,0 +1,9 @@
+package com.damintsev.client.dao;
+
+/**
+ * User: Damintsev Andrey
+ * Date: 30.07.13
+ * Time: 22:55
+ */
+public class Device {
+}
---
+++ src/main/java/com/damintsev/client/frames/Devices.java
@@ -0,0 +1,9 @@
+package com.damintsev.client.frames;
+
+/**
+ * User: Damintsev Andrey
+ * Date: 30.07.13
+ * Time: 22:34
+ */
+public class Devices {
+}
