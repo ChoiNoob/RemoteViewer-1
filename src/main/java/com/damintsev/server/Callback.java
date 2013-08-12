e28b2c351722bfb1c6c38e996b0bec553ffd879d
das <damintsew@gmail.com>
2013-08-12 22:33:06 +0400

    Нормальная реализация клиента для Телнета!!!!!!

---
+++ src/main/java/com/damintsev/server/Callback.java
@@ -0,0 +1,15 @@
+package com.damintsev.server;
+
+/**
+ * User: Damintsev Andrey
+ * Date: 12.08.13
+ * Time: 21:38
+ */
+public abstract class Callback {
+
+    public abstract void onWrite();
+
+    public abstract void onRead();
+
+
+}
