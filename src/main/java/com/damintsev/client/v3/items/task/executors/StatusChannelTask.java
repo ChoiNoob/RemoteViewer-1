9862aca2b8301973d891cda0d6e88b008c0d828f
das <damintsew@gmail.com>
2013-10-14 23:09:35 +0400

    реализована проверка канала

--- src/main/java/com/damintsev/client/v3/items/task/executors/StatusChannelTask.java
+++ src/main/java/com/damintsev/client/v3/items/task/executors/StatusChannelTask.java
@@ -1,6 +1,9 @@
 package com.damintsev.client.v3.items.task.executors;
 
+import com.damintsev.client.v3.items.task.ExecuteState;
 import com.damintsev.client.v3.items.task.TaskState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * User: Damintsev Andrey
@@ -9,8 +12,29 @@ import com.damintsev.client.v3.items.task.TaskState;
  */
 public class StatusChannelTask extends TaskExecutor {
 
+    private static final Logger logger = LoggerFactory.getLogger(StatusChannelTask.class);
+
     @Override
     public TaskState process(String command)  {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        TaskState response = new TaskState();
+
+        logger.info("Executing command=" + command);
+        int index = command.indexOf("PP NW");
+        if (index > 0) {
+            command = command.substring(index, command.length());
+            logger.debug("After substring: " + command);
+        }
+        if (command.contains("READY")) {
+            logger.info("After parse result is WORK");
+            response.setMessage("After parse result is WORK");
+            response.setState(ExecuteState.WORK);
+        } else if (command.contains("NEC")) {
+            logger.info("After parse result is WARNING");
+            response.setState(ExecuteState.WARNING);
+        } else {
+            logger.info("After parse result is ERROR");
+            response.setState(ExecuteState.ERROR);
+        }
+        return response;
     }
 }
